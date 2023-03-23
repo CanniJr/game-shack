17c014371ce101dd9a9238194e3773b7f9983469
Cannigia Laluw <cannigialaluw@gmail.com>
2023-03-23 12:55:17 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
+function App() {
+	return (
+		<div>
+			<p>Hello World!</p>
+			<Button backgroundColor={"blueviolet"}>Press me!</Button>
+		</div>
+	);
+}
+
+export default App;
