7e87804f01aa3e40c5c98106d45a9a4a2afbc3dc
Cannigia Laluw <cannigialaluw@gmail.com>
2023-03-27 23:44:16 -0700

    Added responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
 	return (
-		<div>
-			<p>Hello World!</p>
-			<Button backgroundColor={"blueviolet"}>Press me!</Button>
-		</div>
+		<Grid
+			templateAreas={{
+				base: `"nav" "main"`,
+				lg: `"nav nav" "aside main"`,
+			}}
+		>
+			<GridItem area={"nav"} bgColor="lightblue">
+				Nav
+			</GridItem>
+			<Show above="lg">
+				<GridItem area={"aside"} bgColor="red">
+					Aside
+				</GridItem>
+			</Show>
+			<GridItem area={"main"} bgColor="coral">
+				Test
+			</GridItem>
+		</Grid>
 	);
 }
 
