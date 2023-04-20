d351cbd97703b4ed381fa7038b256827d8c6ecf4
Cannigia Laluw <cannigialaluw@gmail.com>
2023-04-20 13:53:46 -0700

    added cancel handling in useGames hook

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,3 +1,4 @@
+import { CanceledError } from "axios";
 import { useState, useEffect } from "react";
 import apiClient from "../api-client/api-client";
 
@@ -16,10 +17,17 @@ const useGames = () => {
 	const [error, setError] = useState("");
 
 	useEffect(() => {
+		const controller = new AbortController();
+
 		apiClient
-			.get<FetchGameResponse>("/games")
+			.get<FetchGameResponse>("/games", { signal: controller.signal })
 			.then((resp) => setGames(resp.data.results))
-			.catch((err) => setError(err.message));
+			.catch((err) => {
+				if (err instanceof CanceledError) return;
+				setError(err.message);
+			});
+
+		return () => controller.abort();
 	}, []);
 
 	return { games, error };
