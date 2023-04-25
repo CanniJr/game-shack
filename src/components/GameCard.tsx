d058f6f63ae2a80615576ca7919a83b4fb6890f6
Cannigia Laluw <cannigialaluw@gmail.com>
2023-04-24 18:34:41 -0700

    displayed plaatform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,17 +1,21 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import type { Game } from "../hooks/useGames";
+import PlatformIconList from "./PlatformIconList";
 
 interface GameCardProps {
 	game: Game;
 }
 
 const GameCard = ({ game }: GameCardProps) => {
-	const { name, background_image } = game;
+	const { name, background_image, parent_platforms } = game;
 	return (
 		<Card borderRadius="md" overflow="hidden" maxW={"sm"}>
 			<Image src={background_image} h={40} alt="" />
 			<CardBody>
-				<Heading fontSize="md">{name}</Heading>
+				<Heading fontSize="2xl">{name}</Heading>
+				<PlatformIconList
+					platforms={parent_platforms.map((platform) => platform.platform)}
+				/>
 			</CardBody>
 		</Card>
 	);
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,41 @@
+import { Text, Icon, HStack } from "@chakra-ui/react";
+import {
+	FaWindows,
+	FaApple,
+	FaXbox,
+	FaPlaystation,
+	FaAndroid,
+	FaLinux,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { IconType } from "react-icons";
+
+interface PlatformIconProps {
+	platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: PlatformIconProps) => {
+	const iconMap: { [key: string]: IconType } = {
+		pc: FaWindows,
+		mac: FaApple,
+		android: FaAndroid,
+		xbox: FaXbox,
+		playstation: FaPlaystation,
+		linux: FaLinux,
+		ios: MdPhoneIphone,
+		nintendo: SiNintendo,
+		web: BsGlobe,
+	};
+	return (
+		<HStack marginY={1}>
+			{platforms.map((platform) => (
+				<Icon as={iconMap[platform.slug]} color={"gray.500"} />
+			))}
+		</HStack>
+	);
+};
+
+export default PlatformIconList;
