805dc29f85d63f81f147d641fed94de216624ccb
Cannigia Laluw <cannigialaluw@gmail.com>
2023-04-24 19:51:03 -0700

    add url image optimizer

---
+++ src/api-client/image-url.ts
@@ -0,0 +1,9 @@
+const getCroppedImageUrl = (url: string) => {
+	const target = "media/";
+	const index = url.indexOf(target) + target.length;
+	return url.slice(0, index) + "crop/600/400/" + url.slice(index);
+	// some api allows optimized images by adding 'crop' to the url after media/.
+	// this returns a smaller image compared to the original one fetched from the API.
+};
+
+export default getCroppedImageUrl;
--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -2,6 +2,7 @@ import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import type { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
+import getCroppedImageUrl from "../api-client/image-url";
 
 interface GameCardProps {
 	game: Game;
@@ -11,7 +12,7 @@ const GameCard = ({ game }: GameCardProps) => {
 	const { name, background_image, parent_platforms, metacritic } = game;
 	return (
 		<Card borderRadius="md" overflow="hidden" maxW={"sm"}>
-			<Image src={background_image} h={40} alt="" />
+			<Image src={getCroppedImageUrl(background_image)} h={40} alt="" />
 			<CardBody>
 				<Heading fontSize="2xl">{name}</Heading>
 				<HStack justifyContent="space-between">
