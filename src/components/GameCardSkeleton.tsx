4965e5a276107041d1db5fb58cea294d5b1d5ed1
Cannigia Laluw <cannigialaluw@gmail.com>
2023-05-04 23:29:06 -0700

    created loading skeleton

---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,9 @@
+import { Card, Skeleton, CardBody } from "@chakra-ui/react";
+import useGames from "../hooks/useGames";
+import React from "react";
+
+const GameCardSkeleton = () => {
+	return <div>GameCardSkeleton</div>;
+};
+
+export default GameCardSkeleton;
