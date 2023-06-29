c420c6570366296f5a6c535dd2ca6c689841ebe3
Cannigia Laluw <canni@Cannigias-MacBook-Air.local>
2023-06-28 23:47:39 -0700

    refactoring api client

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -5,9 +5,27 @@ export interface FetchResponse<T> {
 	results: T[];
 }
 
-export default axios.create({
+const axiosInstance = axios.create({
 	baseURL: "https://api.rawg.io/api",
 	params: {
 		key: import.meta.env.VITE_RAWG_KEY,
 	},
 });
+
+class APIClient<T> {
+	endpoint: string
+
+	constructor(endpoint : string){
+		this.endpoint = endpoint
+	}
+
+	getAll = () => {
+		return axiosInstance.get<T[]>(this.endpoint).then((res) => res.data)
+	}
+
+	post = (data: T) => {
+		return axiosInstance.post<T>(this.endpoint, data).then((res) => res.data)
+	}
+}
+
+export default APIClient
