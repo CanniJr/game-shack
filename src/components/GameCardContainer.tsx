943d1dd9d349e71d3dacf54fc60db89355d60f59
Cannigia Laluw <cannigialaluw@gmail.com>
2023-07-24 12:33:15 -0700

    add hover transition on gamecard

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -2,15 +2,22 @@ import { Box } from "@chakra-ui/react";
 import { ReactNode } from "react";
 
 interface ContainerProps {
-	children: ReactNode;
+  children: ReactNode;
 }
 
 const GameCardContainer = ({ children }: ContainerProps) => {
-	return (
-		<Box borderRadius={10} overflow="hidden">
-			{children}
-		</Box>
-	);
+  return (
+    <Box
+      borderRadius={10}
+      overflow="hidden"
+      _hover={{
+        transition: "all .1s ease-in-out",
+        transform: "scale(1.05)",
+      }}
+    >
+      {children}
+    </Box>
+  );
 };
 
 export default GameCardContainer;
