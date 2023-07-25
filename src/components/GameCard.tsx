4bbb88037c11dfff9ce29b0b2ea89552fd3766c2
Cannigia Laluw <cannigialaluw@gmail.com>
2023-07-25 15:36:08 -0700

    add game detail page and individual game fetch

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -3,32 +3,34 @@ import type { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "../services/image-url";
-import { Platform } from "../hooks/usePlatforms";
 import GameRankEmoji from "./GameRankEmoji";
-import useGameQueryStore from "../store";
+import { Link } from "react-router-dom";
 
 interface GameCardProps {
   game: Game;
 }
 
 const GameCard = ({ game }: GameCardProps) => {
-  const { name, background_image, parent_platforms, metacritic } = game;
+  const { name, background_image, parent_platforms, metacritic, slug } = game;
+
   return (
-    <Card>
-      <Image src={getCroppedImageUrl(background_image)} h={40} alt="" />
-      <CardBody>
-        <HStack marginBottom={2} justifyContent="space-between">
-          <PlatformIconList
-            platforms={parent_platforms.map((platform) => platform.platform)}
-          />
-          <CriticScore score={metacritic} />
-        </HStack>
-        <Heading fontSize="2xl">
-          {name}
-          <GameRankEmoji rating={game.rating_top} />
-        </Heading>
-      </CardBody>
-    </Card>
+    <Link to={`/games/${slug}`}>
+      <Card>
+        <Image src={getCroppedImageUrl(background_image)} h={40} alt="" />
+        <CardBody>
+          <HStack marginBottom={2} justifyContent="space-between">
+            <PlatformIconList
+              platforms={parent_platforms.map((platform) => platform.platform)}
+            />
+            <CriticScore score={metacritic} />
+          </HStack>
+          <Heading fontSize="2xl">
+            {name}
+            <GameRankEmoji rating={game.rating_top} />
+          </Heading>
+        </CardBody>
+      </Card>
+    </Link>
   );
 };
 
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,5 +1,21 @@
+import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { useParams } from "react-router-dom";
+import useGame from "../hooks/useGame";
+
 const GameDetailPage = () => {
-  return <div>GameDetailPage</div>;
+  const { slug } = useParams();
+  const { data: game, isLoading, error } = useGame(slug!); // the (!) is telling the compiler that the constant will never be null, since this page is only rendered when there is a slug param
+
+  if (isLoading) return <Spinner />;
+
+  if (error || !game) throw error;
+
+  return (
+    <>
+      <Heading mb={5}>{game.name}</Heading>
+      <Text>{game.description_raw}</Text>
+    </>
+  );
 };
 
 export default GameDetailPage;
