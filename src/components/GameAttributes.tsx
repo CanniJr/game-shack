21668f0b2e24ef6f779858e4828f726e99e3f5f4
Cannigia Laluw <cannigialaluw@gmail.com>
2023-07-26 23:08:37 -0700

    adding game attributes

---
+++ src/components/GameAttributes.tsx
@@ -0,0 +1,14 @@
+import { Box, Heading, Text } from "@chakra-ui/react";
+
+const GameAttributes = () => {
+  return (
+    <Box>
+      <Heading size="sm" color="gray">
+        GameAttributes
+      </Heading>
+      <Text>attribute</Text>
+    </Box>
+  );
+};
+
+export default GameAttributes;
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,19 +1,39 @@
-import { Heading, Spinner, Text } from "@chakra-ui/react";
+import { Button, Heading, SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { useState } from "react";
 import { useParams } from "react-router-dom";
+import GameAttributes from "../components/GameAttributes";
 import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!); // the (!) is telling the compiler that the constant will never be null, since this page is only rendered when there is a slug param
+  const [truncate, setTruncate] = useState(true);
 
   if (isLoading) return <Spinner />;
 
   if (error || !game) throw error;
 
+  const arr = [1, 2, 3, 4];
+  console.log(game);
+
   return (
     <>
       <Heading mb={5}>{game.name}</Heading>
-      <Text>{game.description_raw}</Text>
+      <Text noOfLines={truncate ? [1, 2, 3] : 0}>{game.description_raw}</Text>
+      <Button
+        mt={2}
+        mb={5}
+        fontWeight="bold"
+        colorScheme="yellow"
+        onClick={() => setTruncate(!truncate)}
+      >
+        {truncate ? "Show More" : "Show Less"}
+      </Button>
+      <SimpleGrid columns={2} spacing={20}>
+        {arr.map((atr) => (
+          <GameAttributes />
+        ))}
+      </SimpleGrid>
     </>
   );
 };
